43513e22e9196024d383eca930d72faa59c1f3d7
Gopal Jivan <gopal.jivan@gmail.com>
2021-05-11 23:20:15 -0400

    Adding input message

--- udpclient.py
+++ udpclient.py
@@ -1,4 +1,8 @@
 import socket
 
 s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+
+message = input ('Input Lower case message:')
+data = message.encode('ascii')
+s.sendto(data,('127.0.0.1',3000))
 print('OS Provided IP and Port {}'.format(s.getsockname()))
