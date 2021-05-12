a10c4041cee4f21a59a22dbcbe4a16ad813a52ef
Gopal Jivan <gopal.jivan@gmail.com>
2021-05-11 23:37:05 -0400

    Updating to Connect Method

--- udpclient.py
+++ udpclient.py
@@ -1,17 +1,21 @@
 import socket
 
 MAX_SIZE_BYTES = 65535 # Mazimum size of a UDP datagram
+HOST_NAME='127.0.0.1'
+PORT=3000
 
 s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 
 message = input ('Input Lower case message:')
+
+s.connect((HOST_NAME,PORT))
 data = message.encode('ascii')
 
 # Send Data
-s.sendto(data,('127.0.0.1',3000))
+s.send(data)
 print('OS Provided IP and Port {}'.format(s.getsockname()))
 
 # Recieve Data
-data, address = s.recvfrom(MAX_SIZE_BYTES)
+data = s.recv(MAX_SIZE_BYTES)
 response = data.decode('ascii')
-print('Server {} responded with {!r}'.format(address,response))
+print('Server responded with {!r}'.format(response))
