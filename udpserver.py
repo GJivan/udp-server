9250500e8ab53e8c4122143b99d6a951cf444a93
Gopal Jivan <gopal.jivan@gmail.com>
2021-05-11 23:10:56 -0400

    Adding server processing

--- udpserver.py
+++ udpserver.py
@@ -1,5 +1,7 @@
 import socket
 
+MAX_SIZE_BYTES = 65535 # Max Size of UDP Datagram
+
 #Creating Socket
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
@@ -9,4 +11,16 @@ hostname = '127.0.0.1'
 s.bind((hostname,port))
 print(s) # Print Socket Object
 print('Listening at {}'.format(s.getsockname())) # Print IP and Port of Socket
+
+# Listen Forever
+while True:
+    data, clientAddress = s.recvfrom(MAX_SIZE_BYTES) # Wait for Message
+    
+    message = data.decode ('ascii')
+    upperCaseMessage = message.upper()
+    
+    print('Source: {} Message {!r}'.format(clientAddress, message))
+    
+    data =upperCaseMessage.encode('ascii')
+    s.sendto(data,clientAddress)
 
