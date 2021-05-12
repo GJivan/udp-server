494312598b7f7af0a5f0111c4b8d019e0accac82
Gopal Jivan <gopal.jivan@gmail.com>
2021-05-11 23:05:21 -0400

    Binding Socket to Localhost and Port

--- udpserver.py
+++ udpserver.py
@@ -1,4 +1,12 @@
 import socket
 
+#Creating Socket
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-print(s)
+
+port = 3000
+hostname = '127.0.0.1'
+
+s.bind((hostname,port))
+print(s) # Print Socket Object
+print('Listening at {}'.format(s.getsockname())) # Print IP and Port of Socket
+
